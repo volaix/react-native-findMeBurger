fc2d122313a5a4dd6af38d0d3f3c1d617b84790a
Mark David Teo <Mark@Administrators-MacBook-Pro.local>
2017-12-13 17:29:43 +0800

    new component for bottom display + connected to redux for selected map marker

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Dimensions, Text, View, AppRegistry, StyleSheet } from 'react-native';
 import MapViewComponent from './src/components/MapViewComponent'
-
+import SelectionAvatar from './src/components/SelectionAvatar'
 import configureStore from './src/redux/configureStore'
 import { Provider } from 'react-redux'
 
@@ -22,53 +22,7 @@ export default class App extends React.Component {
       }}>
 
       <MapViewComponent />
-
-        {/* Bottom information, Title, Price, image, and Burger */}
-        <View style={{
-          width: 370,
-          top: 630,
-          position: 'relative',
-          flex: 1,
-          alignSelf: 'center',
-          borderRadius: 20,
-          flexDirection: 'row' 
-
-        }}>
-          <View style={{
-            position: 'relative',
-            backgroundColor: 'black',
-            height: 60, 
-            width: 60,
-            top: 20,
-            left: 20,
-          }}>
-          </View>
-
-          <Text style={{
-            backgroundColor: '#fd5c63',
-            position: 'relative',
-            top: 20,
-            left: 20,
-            height: 30,
-            width: 200,
-            color: 'white',
-            fontSize: 18,
-            fontWeight: '800',
-          }}> Jus Burgers
-            </Text>
-
-          <Text style={{
-            backgroundColor: '#fd5c63',
-            position: 'absolute',
-            top: 53,
-            left: 80,
-            height: 20,
-            width: 200,
-            color: 'white',
-          }}> $12.50, Teriyaki Burger
-            </Text>
-
-        </View>
+      <SelectionAvatar />
       </View>
       </Provider>
     );
---
+++ src/components/SelectionAvatar.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { connect } from "react-redux";
+
+  // Bottom information, Title, Price, image, and Burger
+const SelectionAvatar = (props) => (
+  < View style = {{
+          width: 370,
+    top: 630,
+    position: 'relative',
+    flex: 1,
+    alignSelf: 'center',
+    borderRadius: 20,
+    flexDirection: 'row'
+        }}>
+  <View style={{
+    position: 'relative',
+    backgroundColor: 'black',
+    height: 60,
+    width: 60,
+    top: 20,
+    left: 20,
+  }}>
+  </View>
+
+  <Text style={{
+    backgroundColor: '#fd5c63',
+    position: 'relative',
+    top: 20,
+    left: 20,
+    height: 30,
+    width: 200,
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '800',
+  }}> {props.markerSelected}
+            </Text>
+
+  <Text style={{
+    backgroundColor: '#fd5c63',
+    position: 'absolute',
+    top: 53,
+    left: 80,
+    height: 20,
+    width: 200,
+    color: 'white',
+  }}> $12.50, {props.markerSelected}
+            </Text>
+        </View >
+)
+
+const mapStateToProps = (state, props) => {
+    return {
+        markerSelected:  state.mapView.markerSelected,
+    }
+}
+
+export default connect(mapStateToProps)(SelectionAvatar)
